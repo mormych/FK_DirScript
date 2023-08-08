46a965d52a3f8ba4b36ee46028ddad96c7285b2a
Michał Adamiak <michmar2002@gmail.com>
2023-08-08 12:29:55 +0200

    Drobne poprawki - Poprawiono tworzenie katalogów - Poprawiono wyświetlanie komunikatów

--- config_manager.rb
+++ config_manager.rb
@@ -11,6 +11,6 @@
 MAX_NOT_EMPTY_DIRS = 5                            # Maximum number allowed not empty dirs
 CHECK_DIR_COUNT = 10                              # Number of dirs to check
 NEW_DIR_COUNT = 10                                # Dirs will be created if necessary
-WORKING_DIR = 'C:\\Users\\michal\\Desktop\\dtp'   # Working DIR
+WORKING_DIR = 'C:\\Users\\Michal\\Desktop\\dtp'   # Working DIR
 INITIAL_DIR_NUM = 48000                           # First parent_dir
 DIR_LIMIT = 20                                   # Max number of dirs
--- dir_create_delete.rb
+++ dir_create_delete.rb
@@ -4,44 +4,46 @@ require_relative 'config_manager'
 
 def create_initial_structure
   Dir.mkdir INITIAL_DIR_NUM.to_s
-  puts "Entering to work dir..."
+  puts 'Entering to work dir...'
   Dir.chdir INITIAL_DIR_NUM.to_s
   puts "Work dir #{Dir.pwd}"
   (0..NEW_DIR_COUNT - 1).each { |i|
     Dir.mkdir (INITIAL_DIR_NUM + i).to_s
     create_production_structure (INITIAL_DIR_NUM + i).to_s
-    Dir.chdir ".." # Returning to previous directory
+    Dir.chdir '..' # Returning to previous directory
   }
-  puts "done"
+  puts 'done'
 end
 
 def create_production_structure current_dir
   puts "Entering to DIR: #{current_dir}"
   Dir.chdir current_dir
-  puts "Creating structure..."
-  Dir.mkdir "Production"
-  Dir.mkdir "Source"
+  puts 'Creating structure...'
+  Dir.mkdir 'Production'
+  Dir.mkdir 'Source'
+  Dir.mkdir 'Documents'
 end
 
 def check_child_dir parent_dir
   not_empty_dirs = 0
-  puts "Entering to work dir"
+  puts 'Entering to work dir'
   Dir.chdir parent_dir
   puts "Work dir: #{Dir.pwd}"
-  puts "Checking for latest child dir... "
-  puts Dir["*"].last + " Last dir"
-  puts "Dirs count: " + Dir["*"].count.to_s
-  latest_child_dir = (Dir["*"].last).to_i
+  puts 'Checking for latest child dir... '
+  puts Dir['*'].last + ' Last dir'
+  puts 'Dirs count: ' + Dir['*'].count.to_s
+  latest_child_dir = (Dir['*'].last).to_i
   (0..CHECK_DIR_COUNT - 1).each { |i|
-    puts "Latest child dir " + (latest_child_dir - i).to_s
+    puts 'Latest child dir ' + (latest_child_dir - i).to_s
     Dir.chdir (latest_child_dir - i).to_s
     unless content_is_empty?
       not_empty_dirs += 1
-      puts "Katalog nie jest pusty"
+      puts 'Katalog nie jest pusty'
     end
-    Dir.chdir ".."
+    Dir.chdir '..'
     if not_empty_dirs >= MAX_NOT_EMPTY_DIRS
       begin
+        puts 'The number of occupied folders has been exceeded'
         create_child_dirs latest_child_dir
       rescue Exception => e
         puts "Exception: #{e.message}"
@@ -55,9 +57,9 @@ end
 def create_child_dirs latest_child_dir
   puts "jestem tutaj: #{Dir.pwd}"
   puts "Latest child #{latest_child_dir}"
-  (0..NEW_DIR_COUNT - 1).each { |i|
+  (1..NEW_DIR_COUNT).each { |i|
     if latest_child_dir + i > $actual_dir_num + DIR_LIMIT
-      raise Exception, "Przepełnienie katalogu"
+      raise Exception, 'Przepełnienie katalogu'
     end
     if Dir.exist? (latest_child_dir + i).to_s
       next
@@ -65,41 +67,49 @@ def create_child_dirs latest_child_dir
       Dir.mkdir (latest_child_dir + i).to_s
     end
     create_production_structure (latest_child_dir + i).to_s
-    Dir.chdir ".." # Returning to previous directory
+    Dir.chdir '..' # Returning to previous directory
+  }
+end
+
+def create_initial_child_dirs latest_child_dir
+  puts "jestem tutaj: #{Dir.pwd}"
+  puts "Latest child #{latest_child_dir}"
+  (0..NEW_DIR_COUNT - 1).each { |i|
+    if latest_child_dir + i > $actual_dir_num + DIR_LIMIT
+      raise Exception, 'Przepełnienie katalogu'
+    end
+    Dir.mkdir (latest_child_dir + i).to_s
+    create_production_structure (latest_child_dir + i).to_s
+    Dir.chdir '..' # Returning to previous directory
   }
 end
 
 def create_parent_dir
-  Dir.chdir ".."
+  Dir.chdir '..'
   puts "Working directory: #{Dir.pwd}"
-  latest_parent_dir = (Dir["*"].last.to_i) + 1000
+  latest_parent_dir = (Dir['*'].last.to_i) + 1000
   $actual_dir_num = latest_parent_dir
   Dir.mkdir latest_parent_dir.to_s
   Dir.chdir latest_parent_dir.to_s
-  create_child_dirs latest_parent_dir
+  create_initial_child_dirs latest_parent_dir
 end
 
 def content_is_empty?
-  puts "Working dir: " + Dir.pwd
-  puts "Checking dirs for content:"
-  puts "Production empty?: " + Dir.empty?("Production").to_s
-  puts "Source empty?    : " + Dir.empty?("Source").to_s
-  if !Dir.empty? "Production" or !Dir.empty? "Source"
+  puts 'Working dir: ' + Dir.pwd
+  puts 'Checking dirs for content:'
+  puts 'Production empty?: ' + Dir.empty?('Documents').to_s
+  puts 'Production empty?: ' + Dir.empty?('Production').to_s
+  puts 'Source empty?    : ' + Dir.empty?('Source').to_s
+  if !Dir.empty? 'Production' or !Dir.empty? 'Source' or !Dir.empty? 'Documents'
     return false
   end
   true
 end
 
-puts "FK_DirScript"
+puts 'FK_DirScript'
 puts "Started... #{Time.now}"
 
-if ARGV.length.eql? 0
-  puts "Script mode: normal"
-else
-  puts "Script mode: interactive"
-end
-
-puts "Entering to work dir..."
+puts 'Entering to work dir...'
 Dir.chdir WORKING_DIR
 puts "Work dir: #{Dir.pwd}"
 if Dir.empty? Dir.pwd
@@ -107,10 +117,10 @@ if Dir.empty? Dir.pwd
   exit 0
 end
 
-puts "Checking for latest parent dir... "
-puts Dir["*"].last + " Last dir"
-$actual_dir_num = Dir["*"].last.to_i
-check_child_dir(Dir["*"].last).to_s
+puts 'Checking for latest parent dir... '
+puts Dir['*'].last + ' Last dir'
+$actual_dir_num = Dir['*'].last.to_i
+check_child_dir(Dir['*'].last).to_s
 
-puts "All done."
+puts 'All done.'
 exit 0
